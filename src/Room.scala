f6fc34d36964a0ca2b3dd286147dd2a2d1e8750a
Benjamin Parrish <bparrish@trinity.edu>
2017-09-05 14:10:33 -0500

    algoritm for mapfiles

--- src/Room.scala
+++ src/Room.scala
@@ -3,3 +3,24 @@
 class Room {
   
 }
+
+object Room {
+  val rooms = readRoomsFromFile()
+  
+  def readRoomsFromFile():Array[Room] = {
+   val source = io.Source.fromFile("rooms.txt")
+   val lines = source.getLines()
+   val r = Array.fill(lines.next.toInt)(readRoom(lines))
+   source.close
+   r
+  }
+  
+  def readRoom(lines:Iterator[String}): Room = {
+    val name = lines.next()
+    val desc = lines.next()
+    val items = List.fill(lines.next.toInt) {
+      val itm = lines.next.split(";")
+      Item(itm(0). itm(1))
+    }
+  
+}
