f1da90f92b245174f6401a4a2499c232d7c97fcb
Benjamin Parrish <bparrish@trinity.edu>
2017-09-08 16:09:46 -0500

    Adding proper funtion defs

--- src/Player.scala
+++ src/Player.scala
@@ -18,50 +18,21 @@ class Player(
    */
   
   
-  /**
-   * getFromInventory takes String
-   * searches player inventory for an item with matching name
-   * if matching item is found return item. else invalid command
-   */
-  def getFromInventory(itemName: String): Option[Item]{
 
-  }
-  
-  /**
-   * 
-   */
-  def addItem(Item): Unit = {
-    inventory += 
-    }
-  /**
-   * prints whole inventory, includes names and descriptions
-   */
-  def listInventory(){ 
-    for (i <- 0 unitl inventory.length)
-      println(inventory(i).name + ": " + inventory(i).effect)
-  }    
-  /**
-   * command handals all user input
-   * takes string
-   * splits string by spaces
-   * compares case keywords to the elements of input()
-   * to determine the proper funtion to call
-   */
- 
-  def command(String){
+  def command(): Unit = {
     println("Enter a command")
     val input = readLine()
     input.split(" ")
     (input(0)) match{
-      case("move") => input(1) match{
-                        case("up") => player.move(0)
-                        case("down") => player.move(1)
-                        case("east") => player.move(2)
-                        case("west") => player.move(3)
-                        case("north") => player.move(4)
-                        case("south") => player.move(5)
+      case(move) => input(1) match{
+                        case(up) => Player.move(0)
+                        case(down) => Player.move(1)
+                        case(east) => Player.move(2)
+                        case(west) => Player.move(3)
+                        case(north) => Player.move(4)
+                        case(south) => Player.move(5)
                         }
-      case("add") => player.addItem(input(1))
+      case(add) => Player.addItem(input(1))
         
 //			which item(S)?
 //				specify byname
@@ -72,69 +43,103 @@ class Player(
 //				    
 					    
 					
-       case("drop") => player.dropItem(input(1))
+       case(drop) => Player.dropItem(input(1))
 //	 	  remove item form player inventory
 //		    if player.RemoveItem is false
 //		      invalid command
 //		    else
 //		       call currant_room.addItem(dropped item)
 		  
-       case("look") => input(1) match{
-                         case("inventory") => player.printInventory()
-                         case("room") => player.location.printDscrp()
+       case(look) => input(1) match{
+                         case(inventory) => Player.printInventory()
+                         case(room) => Player.location.printDscrp()
                          }         
-       case("exit") => player.exit
+       case(exit) => 
        case(_) => invalid command
        } 
   }
-
-
-  def player.addItem(itemName: String): item {
-  /**
-   * searches player.location.invenotry for Item with same name as aruguement
-   * if Item is found call location.removeItem, else invalid
-   * if lotion.removeItem returns Item add item to player inventory else invalid
-   * 
-   */
-    for each item in inventory
-      if itemName == Item.name 
-      
-        location.removeItem
-        return itemName
-      else 
-        return invalid command
-  }
-
-  def player.removeItem(Item); {
   
+  def move(args: Int): Option[Int] = {
+    ???    
   }
   
-object Player
-/**
- * player constructor 
- * name = keyboard input
- * inventory is empty
- * location set to Inn Room
- */
-def constructPlayer{
-  name = keyboard input
-  //health = 100
-  inventory = nothing
-  location = defualt room (Inn)
  /**
-  * exit function
-  * lets player leave the game if location = the Inn Room 
-  */
-def exit() 
-  if player.location.name == "Inn"
-    terminate aplication 
-  else 
-    print invalid command
-
-  
-}
-
-
-
-
+   * getFromInventory takes String
+   * searches player inventory for an item with matching name
+   * if matching item is found return item. else invalid command
+   */
+  def getFromInventory(itemName: String): Option[Item] = {
+    ???
   }
+////  
+////  /**
+////   * 
+////   */
+  def addItem(args: Item): Unit = {
+    ??? 
+    }
+////  /**
+////   * prints whole inventory, includes names and descriptions
+////   */
+//  def listInventory(){ 
+//    for (i <- 0 unitl inventory.length)
+//      println(inventory(i).name + ": " + inventory(i).effect)
+//  }    
+//  /**
+//   * command handals all user input
+//   * takes string
+//   * splits string by spaces
+//   * compares case keywords to the elements of input()
+//   * to determine the proper funtion to call
+//   */
+// 
+//
+//  def player.addItem(itemName: String): item {
+//  /**
+//   * searches player.location.invenotry for Item with same name as aruguement
+//   * if Item is found call location.removeItem, else invalid
+//   * if lotion.removeItem returns Item add item to player inventory else invalid
+//   * 
+//   */
+//    for each item in inventory
+//      if itemName == Item.name 
+//      
+//        location.removeItem
+//        return itemName
+//      else 
+//        return invalid command
+//  }
+//
+//  def player.removeItem(Item); {
+//  
+//  }
+}
+//  
+//object Player
+///**
+// * player constructor 
+// * name = keyboard input
+// * inventory is empty
+// * location set to Inn Room
+// */
+//def constructPlayer{
+//  name = keyboard input
+//  //health = 100
+//  inventory = nothing
+//  location = defualt room (Inn)
+// /**
+//  * exit function
+//  * lets player leave the game if location = the Inn Room 
+//  */
+//def exit() 
+//  if player.location.name == "Inn"
+//    terminate aplication 
+//  else 
+//    print invalid command
+//
+//  
+//}
+//
+//
+//
+//
--- src/Room.scala
+++ src/Room.scala
@@ -8,10 +8,11 @@ case class Room (
     neighbors: Array [String])
     {
 
- def addItem(Item:) Unit {
-   add Item to list
-}
- def removeItem(Int)
+
+// def addItem(Item:) Unit {
+//   add Item to list
+//}
+ //def removeItem(Int)
    /**
     * remove Item from room inventory
     */
@@ -19,39 +20,43 @@ case class Room (
  /**
   * print room description
   */
- def print info
-   room description 
-   print room inventory
-     for each item in inventory
-       print item.name
-       print item.dscrp
-   print all nonempty neighbors
-     for 
-  
-}
+// def print info {
+//   room description 
+//   print room inventory
+//     for each item in inventory
+//       print item.name
+//       print item.dscrp
+//   print all nonempty neighbors
+//     for 
 
+  
 /**
  * takes room input from file
  */
 
+    }
 
 object Room {
-  val rooms = readRoomsFromFile()
   
-  def readRoomsFromFile():Array[Room] = {
-   val source = io.Source.fromFile("rooms.txt")
-   val lines = source.getLines()
-   val r = Array.fill(lines.next.toInt)(readRoom(lines))
-   source.close
-   r  
+  val directions = List("North", "South", "East", "West", "Up", "Down")
+  directions.foreach(println)
+
+//  val rooms = readRoomsFromFile()
+//  
+//  def readRoomsFromFile():Array[Room] = {
+//   val source = io.Source.fromFile("rooms.txt")
+//   val lines = source.getLines()
+//   val r = Array.fill(lines.next.toInt)(readRoom(lines))
+//   source.close
+//   r  
+//  }
+//  def readRoom(lines:Iterator[String]): Room = {
+//    val name = lines.next()
+//    val dscrp = lines.next()
+//    val items = List.fill(lines.next.toInt) {
+//      val itm = lines.next.split(";") 
+//      //for each item call item constructor
+//      Item(itm(0). itm(1))
+//    }
+//  
   }
-  def readRoom(lines:Iterator[String}): Room = {
-    val name = lines.next()
-    val dscrp = lines.next()
-    val items = List.fill(lines.next.toInt) {
-      val itm = lines.next.split(";") 
-      //for each item call item constructor
-      Item(itm(0). itm(1))
-    }
-  
-}
